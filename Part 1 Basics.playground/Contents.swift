7a603dd9a343653ac653d6b801005b5f32e4baa3
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-07-02 00:15:39 +0300

    complete Part 1 Basics

--- Part 1 Basics.playground/Contents.swift
+++ Part 1 Basics.playground/Contents.swift
@@ -7,6 +7,19 @@ var value2 = 145
 var result = value1 * value2
 var age = 16
 age
-let name = "Anvar"
+//: это - *жирный текст*
+//let name = "Anvar"
+// комментарий
+
+print("Hello, Junior Swift Developer!")
+
+print("Мне \(age) лет")
 
 
+//task 1
+let myConst = "this is my first const"
+//task 2
+var myVariable = 1408
+//task 3 - theory
+//task 4
+print(myConst)
