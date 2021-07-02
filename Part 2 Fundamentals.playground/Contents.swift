7cc1a27b96a55949b237d8b3d55fab13cee4ea01
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-07-02 19:46:32 +0300

    Part 2 Fundamentals

---
+++ Part 2 Fundamentals.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+let someText = "I'm studying Swift!"
+var someText2: String = "I'm studying Swift again!"
+
+let age: Int
+age = 16
+
+var variableOne = 15.0
+var variableTwo: Int = Int(variableOne)
+
+var someNumberString: String = "15"
+var someNumber = Int(someNumberString)
+
+var Num: Int = -15
+var UNum: UInt = 15
+var Num64: Int64 = 99999999999999
+var Num32: Int32 = 9999999
+
+let minInt8 = Int8.min
+let minInt16 = Int16.min
+let minInt32 = Int32.min
+let minInt64 = Int64.min
+
+let minUInt8 = UInt8.min
+let maxUInt8 = UInt8.max
+
+let numFloat: Float = 104.3
+let numDouble: Double = 104.4342423523523
+
+var number = 5
+number += 5
+number *= number
+
+var numberDouble = 5.0
+numberDouble += 3
+numberDouble *= 6
+numberDouble /= 3
+
+let firstFloat: Float = 3.14
+let secondFloat: Float = 1.01
+let result: Float = firstFloat.truncatingRemainder(dividingBy: secondFloat)
+
+var numberEven = 6
+var even: Bool = numberEven.isMultiple(of: 2)
+
