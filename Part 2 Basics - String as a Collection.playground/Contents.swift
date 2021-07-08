1edfe8aa7db6b32e47b50010b51739f04041b3a9
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-07-08 22:36:06 +0300

    complete Part 2 Basics - String as a Collection

--- Part 2 Basics - String as a Collection.playground/Contents.swift
+++ Part 2 Basics - String as a Collection.playground/Contents.swift
@@ -2,3 +2,61 @@ import UIKit
 
 let str = "123"
 str.count
+
+//unicod scalar
+
+let char: Character = "\u{E9}"
+let anotherChar: Character = "\u{65}\u{301}"
+anotherChar == char
+
+//emoji
+
+let thumbsUp = "\u{1f44d}" // "     "
+let skinColor = "\u{1f3fd}" // "
+let combine = "\u{1f44d}\u{1f3fd}" // "   "
+
+//
+
+let cafeSimple = "caf\u{E9}"
+let cafeCombine = "cafe\u{301}"
+cafeSimple.count
+cafeCombine.count 
+//startIndex
+
+let name = "e\u{301}lastic"
+let index = name.startIndex
+let firstChar = name[index]
+type(of: firstChar)
+type(of: index)
+
+//endIndex
+
+let nextChar = name.endIndex
+let lastChar = name.index(before: nextChar)
+name[lastChar]
+
+let secondChar = name.index(after: index)
+name[secondChar]
+
+//offSetBy
+
+let fourthChar = name.index(name.startIndex, offsetBy: 3)
+name[fourthChar]
+
+//unicodeScalars
+
+name.count
+name.unicodeScalars.count
+
+//substring
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz"
+let firstCharIndex = alphabet.startIndex
+let fourthCharIndex = alphabet.index(firstCharIndex, offsetBy: 3)
+let subAlphabet = alphabet[firstCharIndex...fourthCharIndex]
+type(of: subAlphabet)
+let strSubAlphabet = String(subAlphabet)
+type(of: strSubAlphabet)
+
+let subRestOfAlphabet = alphabet[fourthCharIndex...]
+
