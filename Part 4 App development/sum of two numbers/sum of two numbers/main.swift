7af5edc069e0dce8015d76b6abcad60744352fe1
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-07-17 23:02:08 +0300

    complete Part 4 App development - sum of two numbers

---
+++ Part 4 App development/sum of two numbers/sum of two numbers/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  sum of two numbers
+//
+//  Created by Anvar Rahimov on 17.07.2021.
+//
+
+import Foundation
+
+print("Enter first number")
+var firstNumber = readLine()
+print("Enter second number")
+var secondNumber = readLine()
+let result = sum(firstNumber: firstNumber, secondNumber: secondNumber)
+print("The sum of two numbers is \(result)")
