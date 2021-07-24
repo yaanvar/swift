e7f29633750659c1ff0573d68a6e31c8799935cb
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-07-24 22:28:58 +0300

    complete Part 5 Non Trival - Control of access

--- Part 5 Non Trivial/Control of access.playground/Contents.swift
+++ Part 5 Non Trivial/Control of access.playground/Contents.swift
@@ -1,2 +1,24 @@
 import UIKit
 
+ public class User { //everything in swift is internal which means it's working in module's files where it was initialized
+    init() {}
+}
+
+open class UserManager { //open - fully open everywhere for everyone for everything
+    public var currentUser: User //public - open only in module where it's workgng
+    public func loadUser() {
+        
+    }
+    public func createUser(withLogin login: String) {
+    
+    }
+    private func saveToStorage() { //private works only in construction where it was initialized
+    
+    }
+    private func generatePassword() {
+    
+    }
+    init (someUser: User) {
+        currentUser = someUser
+    }
+}
