a534c27c3272eafe674574b39cdc638075cd166a
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-08-04 02:57:37 +0300

    file management 3

---
+++ Part 4 Application development/sum of two numbers/sum of two numbers/func sum.swift
@@ -0,0 +1,12 @@
+//
+//  func sum.swift
+//  sum of two numbers
+//
+//  Created by Anvar Rahimov on 17.07.2021.
+//
+
+import Foundation
+
+func sum(firstNumber: String?, secondNumber: String?) -> Int {
+    return Int(firstNumber!)! + Int(secondNumber!)!
+}
