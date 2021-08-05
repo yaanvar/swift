7d6fc2b7b3d64725aab2a53494f3bf5d9975caac
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-08-05 11:27:18 +0300

    Part 6 App-development

---
+++ Part 6 App-development/MyName/MyName/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyName
+//
+//  Created by Anvar Rahimov on 04.08.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Part 6 App-development/MyName/MyName/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MyName
+//
+//  Created by Anvar Rahimov on 04.08.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
