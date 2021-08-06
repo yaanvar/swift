ea323f13c9732f61746e9d7b15c46f193b7cd4fa
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-08-06 09:58:22 +0300

    RGBApp created

---
+++ Part 6 App-development/RGBApp/RGBApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBApp
+//
+//  Created by Anvar Rahimov on 06.08.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
