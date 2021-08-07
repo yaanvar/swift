7c196aaec92a74bf2b0772c3033aaea3e595a16b
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-08-07 19:37:37 +0300

    lecture 1 - getting started

--- stanford course/Memorize/Memorize/ContentView.swift
+++ stanford course/Memorize/Memorize/ContentView.swift
@@ -9,10 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0)
+                .stroke(lineWidth: 3)
+                    
+            Text("Hello, world!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
