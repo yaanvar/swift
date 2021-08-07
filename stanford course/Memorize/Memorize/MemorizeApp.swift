e678fc4b21c4624ae64824bb41aa6121072e0c15
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-08-07 18:23:51 +0300

    create Memorize project

---
+++ stanford course/Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Anvar Rahimov on 07.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
