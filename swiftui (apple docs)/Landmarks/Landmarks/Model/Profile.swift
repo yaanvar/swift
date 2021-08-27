23dca7ecc10241dc908cd428ad35ef0224d89fa9
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-08-27 20:53:08 +0300

    apple docs: working with ui controls

---
+++ swiftui (apple docs)/Landmarks/Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by Anvar Rahimov on 27.08.2021.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "yaanvar")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { self.rawValue }
+    }
+}
