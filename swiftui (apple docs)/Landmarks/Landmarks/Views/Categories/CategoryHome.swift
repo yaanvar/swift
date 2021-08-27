55bb96494e00f1f03b43ca1e3c947bc2e97662f6
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-08-27 18:09:29 +0300

    apple docs: composing complex interfaces

---
+++ swiftui (apple docs)/Landmarks/Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,38 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Anvar Rahimov on 27.08.2021.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    @EnvironmentObject var modelData: ModelData
+    
+    var body: some View {
+        NavigationView {
+            List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+            }
+            .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+            .environmentObject(ModelData())
+    }
+}
