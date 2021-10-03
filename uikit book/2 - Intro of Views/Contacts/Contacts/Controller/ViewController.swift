d249f48c9c6037b7bed9beef5829556a5729b99e
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-10-03 12:28:35 +0300

    8 - userdefaults

--- uikit book/2 - Intro of Views/Contacts/Contacts/Controller/ViewController.swift
+++ uikit book/2 - Intro of Views/Contacts/Contacts/Controller/ViewController.swift
@@ -8,15 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var storage: ContactStorageProtocol!
+    
     var contacts: [ContactProtocol] = [] {
         didSet {
             contacts.sort{ $0.title < $1.title }
+            storage.save(contacts: contacts)
         }
     }
     private func loadContacts() {
-        contacts.append(Contact(title: "Саня Техосмотр", phone: "+799912312323"))
-        contacts.append(Contact(title: "Владимир Анатольевич", phone: "+781213342321"))
-        contacts.append(Contact(title: "Сильвестр", phone: "+7000911112"))
+        contacts = storage.load()
     }
     
     @IBOutlet var tableView: UITableView!
@@ -53,6 +55,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        storage = ContactStorage()
         loadContacts()
         // Do any additional setup after loading the view.
     }
