7d5930a9775e62bb0e3d5d0ddf1831b273fbaae6
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-10-06 21:57:09 +0300

    transferapp

---
+++ uikit book/3 - Intro of Advanced Views/TransferApp/TransferApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransferApp
+//
+//  Created by Anvar Rahimov on 06.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
