2e5f45749b25f79af7bf5413972edc5388bc8c4a
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-10-07 23:52:20 +0300

    working on TransferApp

--- uikit book/3 - Intro of Advanced Views/TransferApp/TransferApp/ViewController.swift
+++ uikit book/3 - Intro of Advanced Views/TransferApp/TransferApp/ViewController.swift
@@ -13,7 +13,50 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet var dataLabel: UILabel!
 
+    @IBAction func editDataWithProperty(_ sender: UIButton) {
+        // получаем вью контроллер, в который происходит переход
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let editScreen = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        // передаем данные
+        editScreen.updatingData = dataLabel.text ?? ""
+        // переходим к следующему экрану
+        self.navigationController?.pushViewController(editScreen, animated: true)
+    }
 
+    var updatedData: String = "Test data"
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateLabel(withText: updatedData)
+    }
+    // Обновляем данные в текстовой метке
+    private func updateLabel(withText text: String) {
+        dataLabel.text = updatedData
+    }
+
+}
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet var dataTextField: UITextField!
+    var updatingData: String = ""
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateTextFieldData(withText: updatingData)
+    }
+    
+    // обновляем данные в текстовом поле
+    private func updateTextFieldData(withText text: String) {
+        dataTextField.text = text
+    }
+    
+    @IBAction func saveDataWithProperty(_ sender: UIButton) {
+        self.navigationController?.viewControllers.forEach { viewController in
+            (viewController as? ViewController)?.updatedData = dataTextField.text ?? ""
+        }
+    }
 }
 
