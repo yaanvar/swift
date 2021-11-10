b58d14ff27263a91996dfc66a7de39ad24ec0d0a
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-11-10 23:44:11 +0300

    25-27 - swiftbook

--- swift language book/Repeating.playground/Contents.swift
+++ swift language book/Repeating.playground/Contents.swift
@@ -192,5 +192,41 @@ for item in animalsArray {
     }
 }
 
+//MARK: - Any
+
+
+var things = [Any]()
+things.append(0)
+things.append(0.0)
+things.append(42)
+things.append("hello")
+things.append((3.0, 5.0))
+things.append({ (name: String) -> String in "Hello, \(name)" })
+
+for thing in things {
+    switch thing {
+    case let someInt as Int:
+        print("an integer value of \(someInt)")
+    case let someDouble as Double where someDouble > 0:
+        print("a positive double value of \(someDouble)")
+    case let someString as String:
+        print("a string value of \"\(someString)\"")
+    case let (x, y) as (Double, Double):
+        print("an (x, y) point at \(x), \(y)")
+    case let stringConverter as (String) -> String:
+        print(stringConverter("Troll"))
+default:
+        print("something else")
+    }
+}
+
+// AnyObject
+
+let someObjects: [AnyObject] = [Dog(), NoisyDog(), Dog()]
+
+for object in someObjects {
+    let animal = object as! Dog
+    print(animal.type)
+}
 
 
