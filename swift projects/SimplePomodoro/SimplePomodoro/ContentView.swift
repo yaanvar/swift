8d14cec9aa378da4d41cfde8867be786d34a29ad
Anvar <54923760+yaanvar@users.noreply.github.com>
2021-11-20 23:40:29 +0300

    new project SimplePomodoro

---
+++ swift projects/SimplePomodoro/SimplePomodoro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimplePomodoro
+//
+//  Created by Anvar Rahimov on 20.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift projects/SimplePomodoro/SimplePomodoro/SimplePomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplePomodoroApp.swift
+//  SimplePomodoro
+//
+//  Created by Anvar Rahimov on 20.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SimplePomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
