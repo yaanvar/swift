2f0a05db079b7111f58993f5401db37b1abfb051
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-01-20 00:10:24 +0300

    100 days of swift: project 1. day 17

--- 100 days of Swift/Project1/Project1/DetailViewController.swift
+++ 100 days of Swift/Project1/Project1/DetailViewController.swift
@@ -10,13 +10,27 @@ import UIKit
 class DetailViewController:
     UIViewController {
     @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.hidesBarsOnTap = false
+    }
 
     /*
     // MARK: - Navigation
--- 100 days of Swift/Project1/Project1/ViewController.swift
+++ 100 days of Swift/Project1/Project1/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Strom Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
@@ -35,6 +38,14 @@ class ViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+        
     }
 
 }
