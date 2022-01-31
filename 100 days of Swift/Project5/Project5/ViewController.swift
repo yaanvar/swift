8eac3e7f83d377ae17b6f46b106159023a377599
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-01-31 21:14:29 +0300

    100 days of swift: project 4. day 27

---
+++ 100 days of Swift/Project5/Project5/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Anvar Rahimov on 31.01.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return usedWords.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true )
+    }
+    
+    func submit(_ answer: String) {
+        
+    }
+
+}
+
