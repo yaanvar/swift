ac1747922ace396f257ea4ac5b990743a22b0e40
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-02-03 11:49:16 +0300

    100 days of swift: project7. day 34

--- 100 days of Swift/Project7/Project7/ViewController.swift
+++ 100 days of Swift/Project7/Project7/ViewController.swift
@@ -8,19 +8,76 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var storedPetitions = [Petition]()
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        title = "Petitions in US"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Filters", style: .plain, target: self, action: #selector(showFilter))
+        
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
         
+        showError()
+    }
+    
+    @objc func showFilter() {
+        let ac = UIAlertController(title: "Filter", message: "Enter the keywords to filter petitions (Leave empty to reset filters)", preferredStyle: .alert)
+        ac.addTextField()
+        let filterAction = UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+            guard let keyword = ac?.textFields?[0].text else { return }
+            self?.filter(keyword)
+        }
+        ac.addAction(filterAction)
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func filter(_ keyword: String) {
+        filteredPetitions = []
+        for petition in petitions {
+            if keyword == "" {
+                petitions = storedPetitions
+                tableView.reloadData()
+                return
+            }
+            if petition.body.contains(keyword) || petition.title.contains(keyword) {
+                filteredPetitions.append(petition)
+            }
+        }
+        petitions = filteredPetitions
+        tableView.reloadData()
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "The data comes from the We The People API of the Whitehouse", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; Please check your connection and try again", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
@@ -28,6 +85,7 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
+            storedPetitions = petitions
             tableView.reloadData()
         }
     }
@@ -44,6 +102,11 @@ class ViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
 
 }
 
