aef1eab1c6aa9ef1c1b87cefb2f0084bccf8c828
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-02-17 13:11:16 +0300

    100DoS: Project19. day 68

--- 100 days of Swift/Project19/Extension/Action.js
+++ 100 days of Swift/Project19/Extension/Action.js
@@ -2,9 +2,12 @@ var Action = function() {};
 
 Action.prototype = {
     run: function(parameters) {
-        
-    }, finalize: function(parameters) {
-    
+        parameters.completionFunction({"URL": document.URL, "title": document.title });
+    },
+
+    finalize: function(parameters) {
+        var customJavaScript = parameters["customJavaScript"];
+        eval(customJavaScript);
     }
 };
 
