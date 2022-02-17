fb4bf29422b437f880d45a78abd8fe9487609447
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-02-17 12:29:09 +0300

    100DoS: Project19. day 67

---
+++ 100 days of Swift/Project19/Extension/Action.js
@@ -0,0 +1,11 @@
+var Action = function() {};
+
+Action.prototype = {
+    run: function(parameters) {
+        
+    }, finalize: function(parameters) {
+    
+    }
+};
+
+var ExtensionPreprocessingJS = new Action
