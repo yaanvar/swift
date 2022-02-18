59aaba0809ac392a9acf6a5b85be3beae98b5388
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-02-19 01:02:23 +0300

    100DoS: Project20. day 70

---
+++ 100 days of Swift/Project20/Project20/GameScene.swift
@@ -0,0 +1,147 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Anvar Rahimov on 19.02.2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    var gameTimer: Timer?
+    var fireworks = [SKNode]()
+    var scoreLabel: SKLabelNode!
+    
+    let leftEdge = -22
+    let bottomEdge = -22
+    let rightEdge = 1024 + 22
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
+    }
+    
+    @objc func launchFireworks() {
+        let movementAmount: CGFloat = 1800
+        
+        switch Int.random(in: 0...3) {
+        case 0:
+            createFirework(xMovement: 0, x: 512, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 - 200, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 - 100, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 + 200, y: bottomEdge)
+            createFirework(xMovement: 0, x: 512 + 100, y: bottomEdge)
+        case 1:
+            createFirework(xMovement: 0, x: 512, y: bottomEdge)
+            createFirework(xMovement: -200, x: 512 - 200, y: bottomEdge)
+            createFirework(xMovement: -100, x: 512 - 100, y: bottomEdge)
+            createFirework(xMovement: 100, x: 512 + 100, y: bottomEdge)
+            createFirework(xMovement: 200, x: 512 + 200, y: bottomEdge)
+        case 2:
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 100)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 200)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 300)
+            createFirework(xMovement: movementAmount, x: leftEdge, y: bottomEdge + 400)
+        case 3:
+            createFirework(xMovement: movementAmount, x: rightEdge, y: bottomEdge)
+            createFirework(xMovement: movementAmount, x: rightEdge, y: bottomEdge + 100)
+            createFirework(xMovement: movementAmount, x: rightEdge, y: bottomEdge + 200)
+            createFirework(xMovement: movementAmount, x: rightEdge, y: bottomEdge + 300)
+            createFirework(xMovement: movementAmount, x: rightEdge, y: bottomEdge + 400)
+        default:
+            break
+        }
+    }
+    
+    func createFirework(xMovement: CGFloat, x: Int, y: Int) {
+        let node = SKNode()
+        node.position = CGPoint(x: x, y: y)
+        
+        let firework = SKSpriteNode(imageNamed: "rocket")
+        firework.colorBlendFactor = 1
+        firework.name = "firework"
+        node.addChild(firework)
+        
+        switch Int.random(in: 0...2) {
+        case 0:
+            firework.color = .cyan
+        case 1:
+            firework.color = .green
+        case 2:
+            firework.color = .red
+        default:
+            break
+        }
+        
+        let path = UIBezierPath()
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: xMovement, y: 1000))
+        
+        let move = SKAction.follow(path.cgPath, asOffset: true, orientToPath: true, speed: 200)
+        node.run(move)
+        
+        if let emitter = SKEmitterNode(fileNamed: "fuse") {
+            emitter.position = CGPoint(x: 0, y: -22)
+            node.addChild(emitter)
+        }
+        
+        fireworks.append(node)
+        addChild(node)
+    }
+    
+    func checkTouches(_ touches: Set<UITouch>) {
+        guard let touch = touches.first else { return }
+        
+        let location = touch.location(in: self)
+        let nodesAtpoint = nodes(at: location)
+        
+        for case let node as SKSpriteNode in nodesAtpoint  {
+            guard node.name == "firework" else { continue }
+            
+            for parent in fireworks {
+                guard let firework = parent.children.first as? SKSpriteNode else { continue }
+                
+                if firework.name == "selected" && firework.color != node.color {
+                    firework.name = "firework"
+                    firework.colorBlendFactor = 1
+                }
+            }
+            
+            node.name = "selected"
+            node.colorBlendFactor = 0
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        checkTouches(touches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        checkTouches(touches)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        for (index, firework) in fireworks.enumerated().reversed() {
+            if firework.position.y > 900 {
+                fireworks.remove(at: index)
+                firework.removeFromParent()
+            }
+        }
+    }
+    
+}
