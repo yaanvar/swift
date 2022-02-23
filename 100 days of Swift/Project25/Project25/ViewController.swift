cc81db8833e609f25127646b488070acab8c3a9c
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-02-23 19:18:59 +0300

    100DoS: Project25. day 84

--- 100 days of Swift/Project25/Project25/ViewController.swift
+++ 100 days of Swift/Project25/Project25/ViewController.swift
@@ -22,6 +22,12 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
         
+        let showPeersButton = UIBarButtonItem(title: "Show peers", style: .plain, target: self, action: #selector(showPeers))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let writeMessageButton = UIBarButtonItem(title: "Write a message", style: .plain, target: self, action: #selector(writeMessage))
+        toolbarItems = [showPeersButton, spacer, writeMessageButton]
+        navigationController?.isToolbarHidden = false
+        
         mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
         mcSession?.delegate = self
         
@@ -32,6 +38,51 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     }
     
     //MARK: - funcs
+    
+    @objc func showPeers() {
+        guard let mcSession = mcSession else { return }
+        
+        let message = "There is \(mcSession.connectedPeers.count) peers connected."
+        
+        let ac = UIAlertController(title: "Peers", message: message, preferredStyle: .actionSheet)
+        
+        for peer in mcSession.connectedPeers {
+            ac.addAction(UIAlertAction(title: "\(peer.displayName)", style: .default))
+        }
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .destructive))
+    
+        present(ac, animated: true)
+    }
+    
+    @objc func writeMessage() {
+        let ac = UIAlertController(title: "Write a messege to peers", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Send", style: .default) { [weak self, weak ac] _ in
+            guard let message = ac?.textFields?[0].text else { return }
+            self?.sendMessage(message)
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .destructive))
+        present(ac, animated: true)
+    }
+    
+    func sendMessage(_ message: String) {
+        guard let mcSession = mcSession else { return }
+        
+        let data = Data(message.utf8)
+        
+        if !mcSession.connectedPeers.isEmpty {
+            if !message.isEmpty {
+                do {
+                    try mcSession.send(data, toPeers: mcSession.connectedPeers, with: .reliable)
+                } catch {
+                    let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    present(ac, animated: true)
+                }
+            }
+        }
+    }
     
     @objc func importPicture() {
         let picker = UIImagePickerController()
@@ -115,6 +166,11 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
         switch state {
         case .notConnected:
+            DispatchQueue.main.async { [weak self] in
+                let ac = UIAlertController(title: "\(peerID.displayName) has disconnected", message: nil, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                self?.present(ac, animated: true)
+            }
             print("Not connected: \(peerID.displayName)")
         case .connecting:
             print("Connecting: \(peerID.displayName)")
@@ -130,6 +186,14 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
             if let image = UIImage(data: data) {
                 self?.images.insert(image, at: 0)
                 self?.collectionView.reloadData()
+            } else {
+                let message = String(decoding: data, as: UTF8.self)
+                let ac = UIAlertController(title: "New message from \(peerID.displayName)", message: message, preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "Reply", style: .default, handler: { _ in
+                    self?.writeMessage()
+                }))
+                ac.addAction(UIAlertAction(title: "Cancel", style: .destructive))
+                self?.present(ac, animated: true)
             }
         }
     }
