3070102ba8b25a936e13f36367d5ce34d22259a6
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-02-23 14:51:24 +0300

    100DoS: Project24. day 80

---
+++ 100 days of Swift/Project24.playground/Contents.swift
@@ -0,0 +1,89 @@
+import UIKit
+
+//Strings are not arrays
+
+let name = "Taylor"
+
+for letter in name {
+    print("Give me a \(letter)!")
+}
+
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+print(name[3])
+
+//Working with strings in swift
+
+var password = "12345"
+password.hasPrefix("123")
+password.hasSuffix("456")
+
+extension String {
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+    
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
+}
+
+let weather = "it's going to rain"
+print(weather.capitalized)
+
+extension String {
+    var capitalizedFitst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+let input = "Swift is like Objective-C without C"
+input.contains("Swift")
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+extension String {
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+input.containsAny(of: languages)
+
+languages.contains(where: input.contains)
+
+//NSAttributedString
+
+let string = "This is a test string"
+
+let attributes: [NSAttributedString.Key: Any] = [
+    .foregroundColor: UIColor.white,
+    .backgroundColor: UIColor.red,
+    .font: UIFont.boldSystemFont(ofSize: 36)
+]
+
+var attributedString1 = NSAttributedString(string: string, attributes: attributes)
+
+attributedString1 = NSMutableAttributedString(string: string)
+
+let attributedString = NSMutableAttributedString(string: string)
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 10, length: 0))
