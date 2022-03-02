7196dd7e89db2280311e500d84f6b38a42e1effb
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-03-02 21:59:42 +0300

    yeah yeah its rearrangements again. surprised?

--- 100 Days of Swift - UIKit/Project32/Project32/ViewController.swift
+++ 100 Days of Swift - UIKit/Project32/Project32/ViewController.swift
@@ -6,14 +6,126 @@
 //
 
 import UIKit
+import SafariServices
+import CoreSpotlight
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    //MARK: - Data
+    
+    var projects = [[String]]()
+    var favourites = [Int]()
+    
+    //MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Loading data
+        
+        projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"])
+        projects.append(["Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"])
+        projects.append(["Project 3: Social Media", "UIBarButtonItem, UIActivityViewController, the Social framework, URL"])
+        projects.append(["Project 4: Easy Browser", "loadView(), WKWebView, delegation, classes and structs, URLRequest, UIToolbar, UIProgressView., key-value observing"])
+        projects.append(["Project 5: Word Scramble", "Closures, method return values, booleans, NSRange"])
+        projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
+        projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
+        projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."])
+        
+        //MARK: - UserDefaults
+        
+        let defaults = UserDefaults.standard
+        if let savedFavourites = defaults.object(forKey: "favorites") as? [Int] {
+            favourites = savedFavourites
+        }
+        
+        // TableView
+        
+        tableView.isEditing = true
+        tableView.allowsSelectionDuringEditing = true
+        
+    }
+    
+    //MARK: - TableViewController
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return projects.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let project = projects[indexPath.row]
+        cell.textLabel?.attributedText = makeAttributedString(title: project[0], subtitle: project[1])
+
+        if favourites.contains(indexPath.row) {
+            cell.editingAccessoryType = .checkmark
+        } else {
+            cell.editingAccessoryType = .none
+        }
+        
+        return cell
     }
 
+    func makeAttributedString(title: String, subtitle: String) -> NSAttributedString {
+        let titleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline), NSAttributedString.Key.foregroundColor: UIColor.purple]
+        let subtitleAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .subheadline)]
 
+        let titleString = NSMutableAttributedString(string: "\(title)\n", attributes: titleAttributes)
+        let subtitleString = NSAttributedString(string: subtitle, attributes: subtitleAttributes)
+
+        titleString.append(subtitleString)
+
+        return titleString
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showTutorial(indexPath.row)
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if favourites.contains(indexPath.row) {
+            return .delete
+        } else {
+            return .insert
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .insert {
+            favourites.append(indexPath.row)
+            index(item: indexPath.row)
+        } else {
+            if let index = favourites.firstIndex(of: indexPath.row) {
+                favourites.remove(at: index)
+                deindex(item: indexPath.row)
+            }
+        }
+
+        let defaults = UserDefaults.standard
+        defaults.set(favourites, forKey: "favorites")
+
+        tableView.reloadRows(at: [indexPath], with: .none)
+    }
+    
+    func index(item: Int) {
+    }
+
+    func deindex(item: Int) {
+    }
+    
+    //MARK: - SFSafariViewController
+    
+    func showTutorial(_ which: Int) {
+        if let url = URL(string: "https://www.hackingwithswift.com/read/\(which + 1)") {
+            let config = SFSafariViewController.Configuration()
+            config.entersReaderIfAvailable = true
+            
+            let vc = SFSafariViewController(url: url, configuration: config)
+            present(vc, animated: true)
+        }
+    }
+    
 }
 
