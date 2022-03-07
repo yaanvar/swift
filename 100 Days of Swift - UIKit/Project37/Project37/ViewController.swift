381bf4a24a52551b0839b16a1faee10ba62373f4
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-03-07 23:44:46 +0300

    initializing project37

---
+++ 100 Days of Swift - UIKit/Project37/Project37/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project37
+//
+//  Created by Anvar Rahimov on 07.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
