3d1047d19909564f2dfe43664d539964b19ad273
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-03-12 18:50:52 +0300

    getting started with chat

---
+++ My Projects/Chat/Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Anvar Rahimov on 12.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
