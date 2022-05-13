4e893e1c3c267df98e7f439ce0b14f46e37a98bd
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-05-13 15:25:44 +0300

    SnapKitDemo init

---
+++ Sample projects/SnapKitDemo/SnapKitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapKitDemo
+//
+//  Created by Anvar Rahimov on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
