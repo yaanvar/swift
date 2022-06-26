e211dd720bb2473019c2e38ce35c0dac0edccdd4
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-06-26 19:56:23 +0300

    urlsession demo init

---
+++ Sample projects/URLSessionDemo/URLSessionDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSessionDemo
+//
+//  Created by Anvar Rahimov on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
