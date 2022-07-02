4581fb2335f16f74bee38a9ec0bf3df30deac6f6
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-07-02 15:17:58 +0300

    Project BetterSleep
    100 Days of SwiftUI. Day 26

---
+++ 100 Days of Swift - SwiftUI/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Anvar Rahimov on 02.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100 Days of Swift - SwiftUI/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Anvar Rahimov on 02.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
+    
+    var body: some View {
+        VStack {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            
+            DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+        }
+        .padding()
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
