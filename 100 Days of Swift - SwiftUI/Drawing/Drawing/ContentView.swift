e5afe7217cccfd0d49449c921e2e0767dc692ccf
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-07-10 12:50:10 +0300

    Project Drawing
    100 Days of SwiftUI. Day 44

--- 100 Days of Swift - SwiftUI/Drawing/Drawing/ContentView.swift
+++ 100 Days of Swift - SwiftUI/Drawing/Drawing/ContentView.swift
@@ -7,6 +7,30 @@
 
 import SwiftUI
 
+struct Flower: Shape {
+    var petalOffset = -20.0
+    var petalWidth =  0.0
+    
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            let rotation = CGAffineTransform(rotationAngle: number )
+            
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
+    }
+}
+
 struct Triangle: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
@@ -43,10 +67,56 @@ struct Arc: InsettableShape {
     }
 }
 
-struct ContentView: View {
+struct ColorCyclingCircle: View {
+    var amount = 0.0
+    var steps = 100
+    
     var body: some View {
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(.blue, lineWidth: 40)
+        ZStack {
+            ForEach(0..<steps) { value in
+                Circle()
+                    .inset(by: Double(value))
+                    .strokeBorder(
+                        LinearGradient(
+                            gradient: Gradient(colors: [
+                                color(for: value, brightness: 1),
+                                color(for: value, brightness: 0.5)
+                            ]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        ),
+                        lineWidth: 2
+                    )
+            }
+        }
+        .drawingGroup()
+    }
+    
+    func color(for value: Int, brightness: Double) -> Color {
+           var targetHue = Double(value) / Double(steps) + amount
+
+           if targetHue > 1 {
+               targetHue -= 1
+           }
+
+           return Color(hue: targetHue, saturation: 1, brightness: brightness)
+       }
+}
+
+struct ContentView: View {
+    
+    @State var petalOffset = -20.0
+    @State var petalWidth =  100.0
+    
+    @State private var colorCycle = 0.0
+
+    var body: some View {
+        VStack {
+            ColorCyclingCircle(amount: colorCycle)
+                .frame(width: 300, height: 300)
+
+            Slider(value: $colorCycle)
+        }
     }
 }
 
