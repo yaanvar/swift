3625281205badf2d498030ac6fbc26f0f02c37d4
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-08-02 16:25:51 +0300

    Project Instafilter
    100 Days of SwiftUI. Day 63

--- 100 Days of Swift - SwiftUI/Instafilter/Instafilter/ContentView.swift
+++ 100 Days of Swift - SwiftUI/Instafilter/Instafilter/ContentView.swift
@@ -5,44 +5,45 @@
 //  Created by Anvar Rahimov on 02.08.2022.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
+    @State private var showingImagePicker = false
     
     var body: some View {
         VStack {
-            Text("Hello, world!")
-                .frame(width: 300, height: 300)
-                .background(backgroundColor)
-                .blur(radius: blurAmount)
-                .onTapGesture {
-                    showingConfirmation = true
-                }
-                .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-                    Button("Red") {
-                        backgroundColor = .red
-                    }
-                    Button("Green") {
-                        backgroundColor = .green
-                    }
-                    Button("Blue") {
-                        backgroundColor = .blue
-                    }
-                    Button("Cancel", role: .cancel) { }
-                } message: {
-                    Text("Select new color")
-                }
+            image?
+                .resizable()
+                .scaledToFit()
             
-            Slider(value: $blurAmount, in: 0...20)
-            
-            Button("Random Blur") {
-                blurAmount = Double.random(in: 0...20)
+            Button("Select Image") {
+                showingImagePicker = true
             }
         }
-        .padding()
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "example") else { return }
+        
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.sepiaTone()
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
+        
+        guard let outputImage = currentFilter.outputImage else  { return }
+        
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
