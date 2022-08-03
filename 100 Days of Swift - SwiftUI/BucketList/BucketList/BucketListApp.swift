1913f9c87d80c1e6a3844c45ea6949001dc00336
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-08-03 10:28:02 +0300

    Project BucketList
    100 Days of SwiftUI. Day 68

---
+++ 100 Days of Swift - SwiftUI/BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Anvar Rahimov on 03.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100 Days of Swift - SwiftUI/BucketList/BucketList/ContentView.swift
@@ -0,0 +1,71 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Anvar Rahimov on 03.08.2022.
+//
+
+import SwiftUI
+
+struct User: Identifiable, Comparable {
+    static func < (lhs: User, rhs: User) -> Bool {
+        lhs.lastName < rhs.lastName
+    }
+    
+    let id = UUID()
+    let firstName: String
+    let lastName: String
+}
+
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+struct ContentView: View {
+    let users = [
+        User(firstName: "Arnold", lastName: "Rimmer"),
+        User(firstName: "Kristine", lastName: "Kochanski"),
+        User(firstName: "David", lastName: "Lister")
+    ].sorted()
+    
+    var loadingState = LoadingState.loading
+    
+    var body: some View {
+        if loadingState == .loading {
+            LoadingView()
+        } else if loadingState == .success {
+            SuccessView()
+        } else if loadingState == .failed {
+            FailedView()
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
