25fcae2b7801e62921fbcf935276a7d9f100d50c
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-08-04 13:07:08 +0300

    Accessibility update
    100 Days of SwiftUI. Day 75

--- 100 Days of Swift - SwiftUI/WordScramble/WordScramble/ContentView.swift
+++ 100 Days of Swift - SwiftUI/WordScramble/WordScramble/ContentView.swift
@@ -36,6 +36,9 @@ struct ContentView: View {
                             Image(systemName: "\(word.count).circle")
                             Text(word)
                         }
+                        .accessibilityElement()
+                        .accessibilityLabel(word)
+                        .accessibilityHint("\(word.count) letters")
                     }
                 }
                 
