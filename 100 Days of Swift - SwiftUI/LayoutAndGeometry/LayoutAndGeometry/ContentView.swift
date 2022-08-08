3a6ec74477415973f83d5a741c143951d8e1b1cb
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-08-08 14:25:50 +0300

    Project LayoutAndGeometry
    100 Days of SwiftUI. Day 94

--- 100 Days of Swift - SwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 100 Days of Swift - SwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -8,19 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+    
+    @State private var fadeInOut = false
+
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 0) {
-                ForEach(1..<20) { num in
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
                     GeometryReader { geo in
-                        Text("Number \(num)")
-                            .font(.largeTitle)
-                            .padding()
-                            .background(.red)
-                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
-                            .frame(width: 200, height: 200)
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .onAppear {
+                                withAnimation(.easeInOut(duration: 2)) {
+                                    fadeInOut.toggle()
+                                }
+                            }
+                            .opacity(fadeInOut ? 0 : 1)
                     }
-                    .frame(width: 200, height: 200)
+                    .frame(height: 40)
                 }
             }
         }
