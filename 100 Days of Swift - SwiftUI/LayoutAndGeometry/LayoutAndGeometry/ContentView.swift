c241de7d8a6715cfbb20c87cff89c1bbe71339d0
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-08-08 13:58:51 +0300

    Project LayoutAndGeometry
    100 Days of SwiftUI. Day 93

--- 100 Days of Swift - SwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 100 Days of Swift - SwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,17 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            ForEach(0..<10) { position in
-                Text("Number \(position)")
-                    .alignmentGuide(.leading) { _ in
-                        CGFloat(position) * -10
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 0) {
+                ForEach(1..<20) { num in
+                    GeometryReader { geo in
+                        Text("Number \(num)")
+                            .font(.largeTitle)
+                            .padding()
+                            .background(.red)
+                            .rotation3DEffect(.degrees(-geo.frame(in: .global).minX) / 8, axis: (x: 0, y: 1, z: 0))
+                            .frame(width: 200, height: 200)
                     }
+                    .frame(width: 200, height: 200)
+                }
             }
-       }
-       .background(.red)
-       .frame(width: 400, height: 400)
-       .background(.blue)
+        }
     }
 }
 
