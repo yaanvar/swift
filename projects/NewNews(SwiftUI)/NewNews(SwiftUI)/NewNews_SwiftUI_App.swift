3ff139b7f6bb188cb4290effac5c0703ba955af8
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-08-13 19:26:07 +0300

    NewNews: init

---
+++ projects/NewNews(SwiftUI)/NewNews(SwiftUI)/NewNews_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  NewNews_SwiftUI_App.swift
+//  NewNews(SwiftUI)
+//
+//  Created by Anvar Rahimov on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct NewNews_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
