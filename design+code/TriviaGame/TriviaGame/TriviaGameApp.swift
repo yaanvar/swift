ea5fb7cade20654b074d99c2ff84ce615a9386d0
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-09-27 00:38:01 +0300

    TriviaGame init

---
+++ design+code/TriviaGame/TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Anvar Rahimov on 27.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
