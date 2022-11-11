68840e5234471978a5f5f9889c3b4ebcb022cbf8
yaanvar <54923760+yaanvar@users.noreply.github.com>
2022-11-11 16:29:54 +0300

    algo: queue update

---
+++ algo/algo.playground/Pages/Queue.xcplaygroundpage/Sources/QueueStack.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+public struct QueueStack<T>: Queue {
+    private var leftStack: [T] = []
+    private var rightStack: [T] = []
+    
+    public init() {}
+    
+    public var isEmpty: Bool {
+        leftStack.isEmpty && rightStack.isEmpty
+    }
+    
+    public var peek: T? {
+        !leftStack.isEmpty ? leftStack.last : nil
+    }
+    
+    public mutating func enqueue(_ element: T) -> Bool {
+        rightStack.append(element)
+        return true
+    }
+    
+    public mutating func dequeue() -> T? {
+        if leftStack.isEmpty {
+            leftStack = rightStack.reversed()
+            rightStack.removeAll()
+        }
+        return leftStack.popLast()
+    }
+}
+
+extension QueueStack: CustomStringConvertible {
+    public var description: String {
+        String(describing: leftStack.reversed() + rightStack)
+    }
+}
